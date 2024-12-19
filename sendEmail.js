e1a0cfd77a83649ec46edf359bdef74a212644f8
Raul Vega <raul.vega.dv@gmail.com>
2024-12-19 14:22:08 +0100

    Adding git ignore and script.js. Installing node_mailer

--- sendEmail.js
+++ sendEmail.js
@@ -1,1 +1,29 @@
+const nodemailer = require('nodemailer');
 
+// Replace with your email service credentials
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER, // Your email
+        pass: process.env.EMAIL_PASS  // Your email password
+    }
+});
+
+const sendEmail = (to, subject, text) => {
+    const mailOptions = {
+        from: process.env.EMAIL_USER,  // Sender's email
+        to: to,                        // Receiver's email
+        subject: subject,              // Subject of the email
+        text: text                     // Body of the email
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Email sent: ' + info.response);
+    });
+};
+
+// Example usage
+sendEmail('raul.vega.dv@gmail.com', 'Test Subject', 'Test Email Body');
